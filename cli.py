3fabff39ace1144dfe079b3d7ea97d42c218824c
윤인석 <xjwl0305@gmail.com>
2020-08-27 13:31:59 +0900

    초안

---
+++ cli.py
@@ -0,0 +1,92 @@
+from socket import *
+import sys
+import threading
+from bs4 import BeautifulSoup
+import requests
+import re
+import queue
+
+list_titles_b = []  # 글 제목
+list_content_b = []  # 글 내용
+list_during_b = []  # 기간
+list_page_urls_b = []  # 페이지 url
+list_url_b = []  # 게시글 url
+list_html_b = []  # 글 내용 html
+list_titles_j = []  # 글 제목
+list_content_j = []  # 글 내용
+list_during_j = []  # 기간
+list_page_urls_j = []  # 페이지 url
+list_url_j = []  # 게시글 url
+list_html_j = []  # 글 내용 html
+urls_bok = "http://www.bokjiro.go.kr"
+urls_jung = "https://www.gov.kr"
+HOST = '127.0.0.1'
+PORT = 10000
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+
+def save_content(que, t):
+    while que.qsize() != 0:
+        list_urls = que.get()
+        confirm = list_urls.split(" ")[1]
+        list_urls = list_urls.split(" ")[0]
+        if confirm == 'b':  # 복지로
+            next_urls = urls_bok + list_urls
+            list_url_b.append(next_urls)
+            New_url = requests.get(next_urls)
+            soup = BeautifulSoup(New_url.content, "lxml")
+            titles = str(soup.select('.serviceName'))
+            during = str(soup.select('table tbody tr td')[2])
+            list_during_b.append(re.sub('<.+?>', '', during, 0).strip())  # 태그 지우기
+            content = str(soup.select('.shareServiceCont'))
+            list_html_b.append(content)
+            content2 = re.sub('<.+?>', '', content, 0).strip()  # 태그 지우기
+            list_content_b.append(re.sub('&nbsp; | &nbsp; | \n|\t|\r', '', content2))  # 개행, tab 지우기
+            print(t + " 복지로 " + titles)
+        elif confirm == 'j':  # 정부24
+            next_urls = urls_jung + list_urls
+            list_url_j.append(next_urls)
+            New_url = requests.get(next_urls)
+            soup = BeautifulSoup(New_url.content, "lxml")
+            content = soup.select('body > div.contentsWrap.r2n > .contents > .cont-inner > '
+                                  '.tbl-view.gallery-detail > .view-contents')
+            list_html_j.append(str(content))  # 게시글 html
+            list_content_j.append(re.sub('<.+?>', '', str(content), 0).strip())  # 게시글 내용
+            list_during_j.append(re.sub('<.+?>', '', str(soup.select('.date')), 0).strip())
+            list_titles_j.append(re.sub('<.+?>', '', str(soup.select('.tit2')), 0).strip())
+            list_page_urls_j.append(list_urls)
+            print(t + " 정부 24 " + re.sub('<.+?>', '', str(soup.select('.tit2')), 0).strip())
+
+
+def thread(que):
+    t1 = threading.Thread(target=save_content, args=(que, "쓰레드1"))
+    t2 = threading.Thread(target=save_content, args=(que, "쓰레드2"))
+    t3 = threading.Thread(target=save_content, args=(que, "쓰레드3"))
+    t4 = threading.Thread(target=save_content, args=(que, "쓰레드4"))
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+
+
+if __name__ == "__main__":
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(ADDR)
+    que = queue.Queue()
+    while True:
+        data = clientSocket.recv(8192)
+        if data.decode() == 'finish':
+            break
+        data = data.decode().split("///")
+        data.remove('')
+        for i in data:
+            que.put(i)
+        thread(que)
+        if que.qsize() == 0:
+            print("큐 비음")
+            clientSocket.send("re".encode())
---
+++ srv.py
@@ -0,0 +1,132 @@
+from socket import *
+from bs4 import BeautifulSoup
+import requests
+import re
+import queue
+import threading
+import time
+from datetime import datetime
+from dateutil.relativedelta import *
+
+HOST = '127.0.0.1'
+PORT = 10000
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+keyword = [
+    '저소득',
+    '장애인',
+    '다문화가정',
+    '기초생활수급자',
+    '고령자',
+    '북한이탈주민',
+    '한부모가족',
+    '갱생보호',
+    '난민'
+]
+
+
+def change_date(day1, day2):
+    text3 = day1.split('.')
+    text4 = day2.split('.')
+    year = text3[0]
+    year2 = text4[0]
+    if int(text3[1]) == 1:
+        text3[1] = str(12)
+        year = int(year) - 1
+    else:
+        text3[1] = str(int(text3[1]) - 1)
+    if int(text4[1]) == 1:
+        text4[1] = str(12)
+        year2 = int(year2) - 1
+    else:
+        text4[1] = str(int(text4[1]) - 1)
+    month1 = text3[1]
+    month2 = text4[1]
+    change_date1 = str(year) + '.' + str(month1) + '.' + text3[2]
+    change_date2 = str(year2) + '.' + str(month2) + '.' + text3[2]
+    return change_date1, change_date2
+
+
+def get_url(que, now):
+    url = requests.get("http://www.bokjiro.go.kr/nwel/helpus/welsha/selectWelShaInfoBbrdMngList.do")
+    soup = BeautifulSoup(url.content, "lxml")
+    a = soup.find("a", {"class": "num"})
+    list_page_urls = [a['href']]
+    page_token = list_page_urls[0].split('pageIndex=')[0]
+    last_page = soup.find_all("a", {'class': 'goLast'})
+    last_page_num = last_page[0].get('href').split('pageIndex=')[1]
+    cnt = 1
+    while cnt < int(last_page_num) + 1:
+        urls = "http://www.bokjiro.go.kr"
+        origin_urls = urls + page_token + 'pageIndex=' + str(cnt)
+        page_urls = requests.get(origin_urls)
+        soup = BeautifulSoup(page_urls.content, "lxml")
+        for i in soup.find_all("a", {'class': 'point10'}):
+            que.put(i['href'] + " b")
+        cnt = cnt + 1
+        print(now + " 복지로 " + origin_urls)
+    # 여기까지 복지로
+    page_token0 = "https://www.gov.kr/portal/locgovNews"
+    page_token1 = "?srchOrder=&sido=&signgu=&srchArea=&srchSidoArea=&srchStDtFmt="
+    page_token2 = "&srchEdDtFmt="
+    page_token3 = "&srchTxt="
+    page_token4 = "&initSrch=false&pageIndex="
+    date = datetime.now().date()
+    before_date = str(date + relativedelta(months=-1)).replace('-', '.')
+    date = str(date).replace('-', '.')
+    change_date1, change_date2 = before_date, date
+    start_url = page_token0 + page_token1 + change_date1 + page_token2 + change_date2 + page_token3 + str(
+        keyword[0]) + page_token4 + '1'
+    url = requests.get(start_url)
+    soup = BeautifulSoup(url.content, "lxml")
+    last_page_url = soup.select('.pagination li a')
+    sp = re.split('pageIndex=', str(last_page_url[8]))[1]
+    last_page_num = sp.split('"')[0]
+    for i in soup.find_all("dt", {'class': 'pcb'}):
+        que.put(str(i).split('"')[3] + " j")
+    print(now + " 정부24  ")
+    for term in range(1, 24):
+        if term != 1:
+            change_date1, change_date2 = change_date(change_date1, change_date2)  # 날짜 변경
+        for count in range(2, int(last_page_num) + 1):
+            origin_urls = page_token0 + page_token1 + change_date1 + page_token2 + change_date2 + page_token3 + str(
+                keyword[0]) + page_token4 + str(count)
+            url = requests.get(origin_urls)
+            soup = BeautifulSoup(url.content, "lxml")
+            for i in soup.find_all("dt", {'class': 'pcb'}):
+                que.put(str(i).split('"')[3] + " j")
+            print(now + " 정부24  " + origin_urls)
+    # 여기까지 정부24
+
+
+def socket_network(socket, que, now):
+    # if que.qsize() == 0:
+    #     time.sleep(10)
+    while que.qsize() != 0:
+        result = ""
+        for i in range(0, 10):
+            result += que.get() + "///"
+        socket.send(result.encode())
+        print("소켓 전송 result = " + result)
+        data = socket.recv(256)
+        print("소켓 수신 result = " + data.decode())
+    socket.send("finish".encode())
+
+
+def thread(socket, que):
+    t1 = threading.Thread(target=get_url, args=(que, "큐 쓰레드"))
+    t2 = threading.Thread(target=socket_network, args=(socket, que, "소켓 쓰레드"))
+    t1.start()
+    time.sleep(5)
+    t2.start()
+    t1.join()
+    t2.join()
+
+
+if __name__ == "__main__":
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(ADDR)
+    serverSocket.listen(100)
+    clientSocket, addr_info = serverSocket.accept()
+    que = queue.Queue()
+    thread(clientSocket, que)
